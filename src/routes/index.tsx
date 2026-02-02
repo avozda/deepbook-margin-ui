57d2e0dd13ead7cc8377964ca98d54ae03f09b38
avozda <adamvozda88@seznam.cz>
2026-02-02 15:38:17 +0100

    test commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main class="mx-auto p-4 text-center text-gray-700">
-      <h1 class="text-2xl font-bold">DeepBook UI Demo</h1>
+      <h1 class="text-2xl font-bold">DeepBook UI Demo :)</h1>
     </main>
   );
 }
